7932cd542769f95e6757011e8fc59a027d18c455
Jennifer Johnson <jenjeep7@hotmail.com>
2017-07-22 17:42:06 -0500

    doggie google icon

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,6 +9,8 @@ export class MapComponent implements OnInit {
 
   lat: number = 44.840929;
   lng: number = -93.794512;
+  // zoom: number = 8;
+  icon: string = 'https://d30y9cdsu7xlg0.cloudfront.net/png/327992-200.png';
 
   constructor() { }
 
